aae627443c76b65b75d6129eddc88ee9fb8ef3f4
Sir-wb <william.barthelemy@enoc.fr>
2025-10-20 16:48:57 +0200

    Implemented Rook constructor

--- TDLOG_ChessGame/piece.cpp
+++ TDLOG_ChessGame/piece.cpp
@@ -15,6 +15,9 @@ PieceName Piece::getName() const{
     return name;
 }
 
+
 King::King(Position p, Color c) : Piece(p, king, c){};
 
 Queen::Queen(Position p, Color c) : Piece(p, queen, c){};
+
+Rook::Rook(Position p, Color c) : Piece(p, rook, c){};
