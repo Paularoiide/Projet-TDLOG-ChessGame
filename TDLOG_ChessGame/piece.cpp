b353509339e0d54975607d6f8dc98286e76516f7
Sir-wb <william.barthelemy@enoc.fr>
2025-10-20 18:19:19 +0200

    Added constructor for Bishop, knight, Pawn

--- TDLOG_ChessGame/piece.cpp
+++ TDLOG_ChessGame/piece.cpp
@@ -21,3 +21,9 @@ King::King(Position p, Color c) : Piece(p, king, c){};
 Queen::Queen(Position p, Color c) : Piece(p, queen, c){};
 
 Rook::Rook(Position p, Color c) : Piece(p, rook, c){};
+
+Bishop::Bishop(Position p, Color c) : Piece(p, bishop, c){};
+
+Knight::Knight(Position p, Color c) : Piece(p, knight, c){};
+
+Pawn::Pawn(Position p, Color c) : Piece(p, pawn, c){};
