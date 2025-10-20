1be2e8a0f7604ab2ac466c72ce34c951840d5def
Sir-wb <william.barthelemy@enoc.fr>
2025-10-20 14:21:55 +0200

    Implementation of Piece methods and King constructor

---
+++ TDLOG_ChessGame/piece.cpp
@@ -0,0 +1,18 @@
+#include "piece.h"
+
+Piece::Piece(Position p, PieceName n, Color c){
+    pos = p;
+    name = n;
+    color = c;
+}
+Position Piece::getPosition() {
+    return pos;
+}
+Color Piece::getColor() {
+    return color;
+}
+PieceName Piece::getName() {
+    return name;
+}
+
+King::King(Position p, Color c) : Piece(p, king, c){};
