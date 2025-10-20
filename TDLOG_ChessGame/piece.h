28a6b3162f63bdcb2a5096f04e7c0e87c2194fae
Sir-wb <william.barthelemy@enoc.fr>
2025-10-20 16:32:50 +0200

    Adjusted method on Piece and finished documentation on King

--- TDLOG_ChessGame/piece.h
+++ TDLOG_ChessGame/piece.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "move.h"
 #include "rules.h"
+#include "game.h"
+#include <vector>
 /// @class Piece
 /// @brief Represents a general piece
 /// @details This is the base class for all pieces (classical chess or not)
@@ -23,25 +25,36 @@ class Piece {
     /// @brief get the name of the piece
     /// @return name 
     PieceName getName() const;
-    /// @brief Move the piece to position p and change pos
-    /// @param p positon
-    virtual void MoveTo(Position p);
+    /// @brief get the legal moves of the piece
+    /// @details This is a pure virtual method that must be implemented by all derivated classes
+    /// @param board
+    /// @return a vector of legal moves
+    virtual std::vector<Move> getLegalMoves(const Board& board) const;
+    /// @brief set pos to the Position p
+    /// @param p new position
+    void setPosition(Position p);
     /// @brief Virtual destructor (possibly useless)
     virtual ~Piece() = default;
 };
-
+/// @class King
+/// @brief Derivated classes from Piece
+/// @details Represents a king an the board
 class King : public Piece {
     public:
+    /// @brief Creates a King instance
+    /// @param p Starting position
+    /// @param c color (White or Black)
     King(Position p , Color c);
-    void MoveTo(Position p) override;
+    /// @brief get the legal moves of the king
+    /// @param board 
+    /// @return a vector of legal moves
+    std::vector<Move> getLegalMoves(const Board& board) const override final;
 };
 class Queen : public Piece {
     public:
     Queen(Position p, Color c);
-    void MoveTo(Position p) override;
 };
 class Rook : public Piece {
     public:
     Rook(Position p, Color c);
-    void MoveTo(Position p) override;
 };
