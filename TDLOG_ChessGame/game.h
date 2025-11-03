a10c729fe6df774df7e880032e79a5a2f100d53a
Paul Andreis <paul.andreis@eleves.enpc.fr>
2025-11-03 13:52:10 +0100

    Add variant system

--- TDLOG_ChessGame/game.h
+++ TDLOG_ChessGame/game.h
@@ -1,2 +1,21 @@
 #pragma once
-struct Board; // temporary declaration
+#include <vector>
+#include "rules.h"
+
+class Board; // temporary declaration
+class Piece;
+class Variant;
+class Move;
+class Player;
+
+class Game{
+    Board board;
+    std::vector<Player> players;
+    Variant variant;
+    Color currentTurn;
+
+    void StartGame();
+    void PlayMove(Move move);
+    bool isCheckmate();
+};
+
