c0b709f2a0685155d238b514c9e296f62e717ff6
Sir-wb <william.barthelemy@enoc.fr>
2025-11-03 14:22:42 +0100

    Finish implementing board and square

--- TDLOG_ChessGame/board.h
+++ TDLOG_ChessGame/board.h
@@ -5,14 +5,17 @@
 class Square{
     Position position;
     Piece* piece;
+    public:
     Square(Position p): position(p),piece(nullptr) {}
+    Piece* getPiece() const {return piece; } ;
+    void setPiece(Piece* p ) {piece = p;};
 };
 class Board {
     int size;
     std::vector<Square> squares;
     public:
     Board(int s);
-    Piece* getPiece(Position pos);
+    Piece* getPiece(const Position pos) const;
     void movePiece(Move m);
-    bool hasPiece(Position pos);
+    bool hasPiece(Position pos) const;
 };
