fcb2811196ecec52f55e49b32f8f3b3cdad09fc3
Sir-wb <william.barthelemy@enoc.fr>
2025-11-10 14:23:08 +0100

    Ajout d'une méthode inBounds pour board

--- TDLOG_ChessGame/board.h
+++ TDLOG_ChessGame/board.h
@@ -11,11 +11,16 @@ class Square{
     void setPiece(Piece* p ) {piece = p;};
 };
 class Board {
+    protected:
     int size;
     std::vector<Square> squares;
     public:
+    int getSize() const {return size;};
     Board(int s);
     Piece* getPiece(const Position pos) const;
     void movePiece(Move m);
     bool hasPiece(Position pos) const;
+    bool inBounds(Position pos) const {
+        return (pos.x >= 0 && pos.x < size && pos.y >= 0 && pos.y < size);
+    }
 };
