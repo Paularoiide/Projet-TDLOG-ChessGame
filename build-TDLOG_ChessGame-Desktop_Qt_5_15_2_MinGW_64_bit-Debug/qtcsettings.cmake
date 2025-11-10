6aec15e5197e960c5fb09b0c16221617729a6307
Paul Andreis <paul.andreis@eleves.enpc.fr>
2025-11-10 14:13:46 +0100

    Updates on move

--- build-TDLOG_ChessGame-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/qtcsettings.cmake
+++ build-TDLOG_ChessGame-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -1,10 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/PaulA/OneDrive/Bureau/Projet Chess/Projet-TDLOG-ChessGame/build-TDLOG_ChessGame-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/ENPC/5.15.2/mingw81_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/ENPC/5.15.2/mingw81_64" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/ENPC/Tools/mingw810_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/ENPC/Tools/mingw810_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
